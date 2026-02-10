39256bbe673a05c1b061d6e04a0a93cadedb45ee
skywalker-97 <rajgaurav7250@gmail.com>
2026-02-10 13:10:09 +0530

    First

--- api/index.js
+++ api/index.js
@@ -124,6 +124,8 @@ app.post("/bfhl", async (req, res) => {
   }
 });
 
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
 
 module.exports = app;
